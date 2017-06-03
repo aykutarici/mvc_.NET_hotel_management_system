b0b43fde48eac3adc55a5b7e05e3c821139f35c3
ariciaykut <ariciaykut@bitbucket.com>
2017-06-03 11:50:03 +0300

    as

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MVC_webapp.Controllers
         {
             return View();
         }
-         
+          
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
